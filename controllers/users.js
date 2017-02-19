375600002f0fcb902c498e83b260e152e3dacc79
balsalob <prietobalsalobre@gmail.com>
2017-02-19 12:57:24 +0100

    Añadido sistema de loguin

--- controllers/users.js
+++ controllers/users.js
@@ -19,6 +19,30 @@ function signUp(req,res) {
   });
 }
 
+function signIn(req, res) {
+
+  var params = req.body;
+  var username = params.username;
+  var password = params.password;
+
+  User.findOne({username: username}, function(err, user) {
+    if(err){
+      res.status(500).send({message:'Error en el proceso de loguin'});
+    }else{
+      if(!user){
+        res.status(404).send({message:'El usuario no está registrado'});
+      }else{
+        if(password == user.password){
+          res.status(200).send({user: user});
+        }else{
+          res.status(500).send({message:'Contraseña incorrecta'});
+        }
+      }
+    }
+  });
+
+}
+
 function addFavorite(req, res) {
   var userId = req.params.id;
   var update = reg.body;
@@ -37,7 +61,8 @@ function showFavorite(req, res) {
 }
 
 module.exports = {
-  signUp
-  addFavorite
+  signUp,
+  signIn,
+  addFavorite,
   showFavorite
 };
--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ var express = require('express');
 var userController = require('../controllers/users');
 var api = express.Router();
 
-api.post('/signup', userController.saveUser)
+api.post('/signup', userController.signUp)
+api.post('/signin', userController.signIn)
 
 module.exports = api;
