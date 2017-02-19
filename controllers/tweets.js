5fecca44a40c6144c3ab052ad94cccfc5ae1c165
balsalob <prietobalsalobre@gmail.com>
2017-02-19 19:34:14 +0100

    controlador de tweets

--- controllers/tweets.js
+++ controllers/tweets.js
@@ -1,8 +1,48 @@
 var mongoose = require('mongoose');
-var User  = require('../models/tweet');
+var Tweet  = require('../models/tweet');
 
+function saveTweet(req,res) {
 
+  var tweet = new Tweet();
+  var params = req.body;
+
+  tweet.text = params.text;
+  tweet.user = req.session.user_id;
+
+  tweet.save(function (err, tweetStored) {
+    if(err){
+      res.status(500).send({message:'Error al guardar el tweet'});
+    }else{
+      res.status(200).send({tweet: tweetStored});
+    }
+  });
+}
+
+function getTweets(req,res) {
+  Tweet.find(function(err, tweets) {
+    if(err){
+      res.status(500).send({message:'Error al recuperar los tweets'});
+    }else{
+      res.status(200).send({tweets});
+    }
+  });
+
+}
+
+function getTweet(req,res) {
+  var tweetId = req.params.id;
+
+  Tweet.findById(tweetId, function(err, tweet) {
+    if(err){
+      res.status(500).send({message:'Error al recuperar el tweet'});
+    }else{
+      res.status(200).send({tweet: tweet});
+    }
+  });
+}
 
 module.exports = {
-
+  saveTweet,
+  getTweets,
+  getTweet
 };
--- routes/tweet.js
+++ routes/tweet.js
@@ -1,7 +1,9 @@
 var express = require('express');
-var userController = require('../controllers/users');
+var tweetController = require('../controllers/tweets');
 var api = express.Router();
 
-
+api.post('/sendtweet', tweetController.saveTweet);
+api.get('/', tweetController.getTweets);
+api.get('/tweet/:id', tweetController.getTweet);
 
 module.exports = api;
